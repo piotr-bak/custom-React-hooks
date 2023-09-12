14f45e3b599eaf19ead9fc1f75b344757ef87655
Piotr Bąk <68067947+piotr-bak@users.noreply.github.com>
2023-09-13 00:36:05 +0200

    [update] Added brief readme part

--- useArray/useArray.js
+++ useArray/useArray.js
@@ -1,3 +1,51 @@
+/**
+ * A custom React hook for managing arrays with various array manipulation functions.
+ *
+ * @param {any[]} initialValue - The initial array value.
+ * @returns {Object} An object containing the array, array manipulation functions, and a setter function.
+ *
+ * @example
+ * // Initialize the array hook with an empty array.
+ * const { array, push, pop, shift, unshift, replace, filter, remove, clear, reset } = useArray([]);
+ *
+ * // Initialize the array hook with an array consisting of 3 numbers:
+ * const INIT = [3, 5, 7]
+ * const { array, push, pop, shift, unshift, replace, filter, remove, clear, reset } = useArray(INIT);
+ *
+ * // Push an item to the array.
+ * push('My item');
+ *
+ * // ...and inside a JSX component:
+ * return (
+ *   <>
+ *     <button onClick={() => push('My item')}>Push 'My Item'</button>
+ *   </>
+ *  )
+ *
+ * // Pop the last item from the array.
+ * pop();
+ *
+ * // Shift the first item from the array.
+ * shift();
+ *
+ * // Unshift an item to the beginning of the array.
+ * unshift('item');
+ *
+ * // Replace an item at a specific index.
+ * replace(index, 'newItem');
+ *
+ * // Filter the array based on a callback.
+ * filter(item => item.condition);
+ *
+ * // Remove an item at a specific index.
+ * remove(index);
+ *
+ * // Clear the array.
+ * clear();
+ *
+ * // Reset the array to the initial value.
+ * reset();
+ */
 import { useCallback, useState } from "react";
 
 export function useArray (initialValue) {
